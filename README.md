4a1661307b32ace1e4469989d9acb1f953e15420
Melanie <melaniechele@gmail.com>
2020-08-11 14:47:19 -0400

    finished 10 events

--- README.md
+++ README.md
@@ -27,16 +27,18 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ### Task 2a: Create listeners for 10 types of events
 
 * [ ] Using your [index.js file](js/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
-  * `mouseover`
-  * `keydown`
+  * `mouseover` done mouseenter and mouse leave
+  * `keydown` done 
   * `wheel`
-  * `load`
-  * `focus`
-  * `resize`
+  * `load` done
+  * `focus` done
+  * `resize` done
   * `scroll`
   * `select`
-  * `dblclick`
+  * `dblclick`done 
   * `drag / drop`
+  * `copy` done
+
 
 Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
 
--- js/index.js
+++ js/index.js
@@ -1,28 +1,146 @@
 // Your code goes here
 const allPage = document.querySelector("html");
 
-
-
-allPage.style.backgroundColor = "#6b4b3e";
+allPage.style.textAlign = "center"
+allPage.style.color = 'purple'
 
 window.addEventListener('load', () => {
-    alert("Loaded :)")
+    allPage.style.backgroundColor = 'lightblue'
     });
 
 //centered logo title to screen to make it look better
 
 const topBack = document.querySelector(".logo-heading");
-console.log(topBack);
 
-topBack.style.textAlign = "center"
 
-//event listener to change color of h1
+topBack.addEventListener('mouseenter', () => {
+    topBack.style.fontSize = "80px";
+});
 
-topBack.addEventListener('mouseover', () => {
-    topBack.style.color = "#f8f4f9";
+topBack.addEventListener('mouseleave', () => {
+    topBack.style.fontSize = "50px";
 });
 
 topBack.addEventListener('dblclick', () => {
 alert("Why are you clicking me?!")
 });
+
+// nav (no events)
+const navPart = document.querySelector(".nav");
+navPart.style.padding = '50px';
+
+const navStyle = document.querySelectorAll(".nav-link");
+
+navStyle.forEach((styles) => {
+    styles.style.textDecoration = 'none';
+    styles.style.padding = '20px';
+
+  });
+
+
+
+// image event
+
+
+window.addEventListener('resize', (e) =>{
+    allPage.style.backgroundColor = 'black'
+    allPage.style.color = 'orange'
+})
+
+
+document.addEventListener('keydown', (e) => {
+    console.log(`${e.code}`)
+})
+
+document.addEventListener('select', (e)=>{
+    console.log(`${e.code}`)
+})
+
+allPage.addEventListener('copy', (event) => {
+    const selection = document.getSelection();
+    event.clipboardData.setData('text/plain', selection.toString().toUpperCase());
+    event.preventDefault();
+});
+
+window.addEventListener('focus', (event) => {
+    console.log('here it goes. you-re focusing on something finally!');
+});
+
+
+
+// window.addEventListener('scroll', (event)=>{
+//      // Amount of times scrolled
+//     const  timesScrolled = document.documentElement.scrollHeight - window.innerHeight;
+
+//      // Getting the amount of scrollability 
+//     const screenHeight = window.scrollY; 
+
+//      // checking if we are at the bottom of page
+//     if (screenHeight === timesScrolled ){
+//         alert('This is the bottom');
+//     }
+// })
+
+
+// created elements to drag event
+const divDrag = document.createElement('div');
+
+// adding style to div drag
+divDrag.style.border = '1px solid black'
+divDrag.style.width = '100px'
+
+
+//creating paragraph for diva
+const para = document.createElement('p');
+
+//added text to drag
+para.textContent = "DRAG ME!!!!!"
+
+//adding attribute
+para.setAttribute('draggable', true)
+para.setAttribute('class', "dropTheBomb")
+
+// grabbing container
+const container = document.querySelector('.container')
+
+// appending to container and text created 
+container.appendChild(divDrag);
+divDrag.appendChild(para);
+
+//created div to drop elevent
+const divDrop = document.createElement('div');
+
+//adding attributes 
+divDrop.setAttribute('class','dropTheBomb' )
+
+
+//adding astyle to div grop
+divDrop.style.border = '1px solid black'
+divDrop.style.width = '150px'
+divDrop.style.height = '50px'
+divDrop.style.margin = '15px'
+
+//appending to divDrag
+container.appendChild(divDrop);
+
+//grabbing ondrag 
+/* Events fired on the drag target */
+document.ondragstart = function(event) {
+    event.dataTransfer.setData("Text", event.target.id);
+  };
+
+/* Events fired on the drop target */
+document.ondragover = function(event) {
+    event.preventDefault();
+  };
+
+  document.ondrop = function(event){
+    event.preventDefault();
+    if ( event.target.className == "dropTheBomb" ) {
+      var data = event.dataTransfer.getData("Text");
+      event.target.appendChild(document.getElementById(data));
+  }
+}
+  
+
 
